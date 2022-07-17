1e6c99ceeb235ac1f1d74061a3bba4400a4cebe3
Carlos Dubón <holacarlosdubon@gmail.com>
2022-07-16 18:06:57 -0600

    add wait helper :clock3:

--- src/lib/Toast.svelte
+++ src/lib/Toast.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+  import { wait } from './helpers/wait';
+  import { fade, fly } from 'svelte/transition';
+
   import type { ToastType } from './toast';
 
   export let title = '';
@@ -9,6 +13,30 @@
   let visible = true;
 
   let isHovered = false;
+
+  onMount(async () => {
+    await wait(duration);
+
+    if (!isHovered) {
+      visible = false;
+    }
+  });
 </script>
 
-{#if visible}{/if}
+{#if visible}
+  <div
+    in:fly={{ x: 200, duration: 500 }}
+    out:fade
+    on:mouseenter={() => (isHovered = true)}
+    on:mouseleave={async () => {
+      isHovered = false;
+
+      await wait(500);
+      if (!isHovered) {
+        visible = false;
+      }
+    }}
+  >
+    Hello
+  </div>
+{/if}
