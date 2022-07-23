8901c8978359a59fbaac514ae33f4dd965461087
Carlos Dubón <holacarlosdubon@gmail.com>
2022-07-23 17:26:56 -0600

    v1.0.1

---
+++ src/lib/index.ts
@@ -0,0 +1,8 @@
+import Toast from './Toast.svelte';
+import ToastProvider from './ToastProvider.svelte';
+import { toast } from './toast';
+
+import type { ToastType } from './toast';
+import type { CustomContentProps } from './types/CustomContentProps';
+
+export { Toast, ToastProvider, toast, ToastType, CustomContentProps };
